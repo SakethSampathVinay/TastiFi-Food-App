fe022923716859a065135477ff8da0a4968d8044
Saketh Sampath Vinay <sakethsampath2006@gmail.com>
2024-09-20 21:19:13 +0530

    updated main file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom"
-import StoreContextProvider from './context/StoreContext.jsx'
+import {StoreContextProvider} from './context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
