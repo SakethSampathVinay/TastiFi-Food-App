9c6bcf1d20a9678613c7fdf392dcc7b1d641e48c
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2024-10-18 21:43:46 +0530

    Remove .env file and update admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
 
-  const url = "https://tastifiadmin.vercel.app"
+  const url = "https://tastifiadmin.vercel.app";
 
   return (
     <div>
