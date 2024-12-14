3d15c0431090a38fe0f36a43301f496dfadf5075
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2024-12-14 17:19:02 +0530

    TastiFi Admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,15 +1,14 @@
-import React from "react"
-import Navbar from "./components/Navbar/Navbar"
-import Sidebar from "./components/Sidebar/Sidebar"
-import {Route, Routes} from "react-router-dom"
-import Add from "./pages/Add/Add"
-import List from "./pages/List/List"
-import Orders from "./pages/Orders/Orders"
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Orders from "./pages/Orders/Orders";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const App = () => {
-
-  const url = "http://localhost:5174"; // need to change this
+  const url = "https://tastifiadmin.vercel.app"; // need to change this
 
   return (
     <div>
@@ -19,13 +18,13 @@ const App = () => {
       <div className="app-content">
         <Sidebar />
         <Routes>
-          <Route path = "/add" element = {<Add url = {url}/>} />
-          <Route path = "/list" element = {<List url = {url}/>} />
-          <Route path = "/orders" element = {<Orders url = {url}/>} />
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
         </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
