6a6187e1dd0ad115c3040420682c9bddb7eb01c3
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-03-14 12:45:59 +0530

    Changed Admin Panel Navbar

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,18 @@
-import React  from "react";
-import "./Navbar.css"
-import {assets} from "../../assets/assets"
+import React from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <img className="logo" src={assets.logo} alt="logo" />
-            <img className="profile" src={assets.profile_image} alt="profile image" />
-        </div>
-    )
-}
+  return (
+    <div className="navbar">
+      <img
+        src="https://res.cloudinary.com/dgtfgihga/image/upload/v1727961916/Screenshot_2024-10-03_185337_kfx3hh.png"
+        className="logo"
+      />
+      <p className="active">Admin Panel</p>
+      {/* <img className="logo" src={assets.logo} alt="logo" /> */}
+      <img className="profile" src={assets.profile_image} alt="profile image" />
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
