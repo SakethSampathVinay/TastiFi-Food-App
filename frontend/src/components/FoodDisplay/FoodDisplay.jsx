4bed1b6d24e5a8e254058b06055bd2e202d723ad
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-04-30 10:30:36 +0530

    Added Loading

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,32 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
 const FoodDisplay = ({ category }) => {
+  const [loading, setLoading] = useState(true);
   const { food_list } = useContext(StoreContext);
+
+  useEffect(() => {
+    if (food_list && food_list.length > 0) {
+      setLoading(false);
+    }
+  }, [food_list]);
+
   return (
     <div className="food-display" id="food-display">
-      <h2>Top dishes near you</h2>
-      <div className="food-display-list">
-        {food_list.map((item, index) => {
-          {
-            //console.log(category, item.category);
-          }
-          if (category === "All" || category === item.category) {
-            return (
-              <FoodItem
-                key={index}
-                id={item._id}
-                name={item.name}
-                description={item.description}
-                price={item.price}
-                image={item.image}
-              />
-            );
-          }
-        })}
-      </div>
+      {loading ? (
+        <div className="loading-container">
+          <div className="loading-spinner"></div>
+        </div>
+      ) : (
+        <>
+          <h2>Top dishes near you</h2>
+          <div className="food-display-list">
+            {food_list
+              .filter(
+                (item) => category === "All" || category === item.category
+              )
+              .map((item, index) => (
+                <FoodItem
+                  key={index}
+                  id={item._id}
+                  name={item.name}
+                  description={item.description}
+                  price={item.price}
+                  image={item.image}
+                />
+              ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
